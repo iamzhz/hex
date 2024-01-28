1f288634a7c31bc9d18b08eda438a767ad5a7cca
iamzhz <88195244+iamzhz@users.noreply.github.com>
2024-01-28 13:08:23 +0800

    Update hex.c

--- hex.c
+++ hex.c
@@ -1,35 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "mycon.h"
 
+void showHex(int number);
 int main(int argc, char *argv[])
 {
-	if(argc < 2) return 0;
-	FILE *fp=fopen(argv[1], "rb");
-	if(fp == NULL){
+	char *filename = NULL;
+	int column = 16; // column number
+	int i;
+	int ch;
+	// parse command-line parameters
+	for(i = 1;  i<=(argc-1);  i ++) {
+		if(!strcmp(argv[i], "-f")) {
+			filename = argv[i+1];
+			i ++;
+		}else if(!strcmp(argv[i], "-c")) {
+			column = atoi(argv[i+1]);
+			i ++;
+		}
+	}
+	if(filename == NULL) {
+		printf(" > hex -f [file name] (-c [column number])\n");
+		return 0;
+	}
+
+	FILE *fp=fopen(filename, "rb");
+	if(fp == NULL) {
 		set_color(0x0, 0x4);
-		puts("ERROR");
+		printf("Connot open file `%s`. \n", filename);
 		set_color(0x0, 0xF);
 		return 0;
 	}
-	set_color(0x0, 0x9); //浅蓝
-	puts("00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F");
-	set_color(0x0, 0x1); //蓝色
-	char ch;
-	int i = 0;
+	// show top line
+	set_color(0x0, 0x9);
+	for(i = 0;  i < column;  i++) showHex(i);
+	printf("\n");
+	// show hex
+	set_color(0x0, 0xF);
+	i = 0;
 	ch=fgetc(fp);
-	while(!feof(fp))      
-    {
-        if(ch < 0x10)
-		printf("0%X ", ch);
-		else
-		printf("%X ", ch);
-		
+	while(!feof(fp)) {
+        showHex(ch);
 		ch=fgetc(fp);
-		i += 1;
-		if(i == 0x10){
-			puts(""); //如果一行已经有16个就换行
+		i ++;
+		if(i == column){
+			puts("");
 			i = 0;
 		}
 	}
 	fclose(fp);
-	set_color(0x0, 0xF); //白色（回到默认颜色）
+	set_color(0x0, 0xF);
+	return 0;
+}
+void showHex(int number) {
+	if(number < 0x10) printf("0%X ", number);
+	else printf("%X ", number);
+}
